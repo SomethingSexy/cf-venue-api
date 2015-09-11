946601ae53446864d36827eca0cf00829e2a23d0
Tyler Cvetan <tyler.cvetan@gmail.com>
2015-09-10 20:54:18 -0500

    Updated to add notes on things that need to be completed
    Signed-off-by: Tyler Cvetan <tyler.cvetan@gmail.com>

--- src/resources/venues/controller.js
+++ src/resources/venues/controller.js
@@ -9,23 +9,32 @@ export function* index(next) {
 
 // Get a single job
 export function* venue(next) {
+  // TODO read from database, if expires is older than 30 days, fetch, update, then return
   this.status = 200;
   this.body = [];
   yield next;
 }
 
-// This will create a new venue in our database
-// either from foursquare data or manually
+// This will "create" a new venue in our database
+// cache the external data and then create stubs for our
+// data to go a long with it
+//
+// The request body might have just the external id or it might
+// get the whole external venue data
 export function *create(next) {
   const postData = this.request.body;
   // if external data request
   if (postData.external) {
+    // unless it contains venue information already
     if ((!postData.externalId || !postData.externalSrc)) {
       this.status = 422;
       this.body = {};
       return;
     }
     const externalData = yield getFoursquareVenue(postData.externalId);
+
+    // TODO Save to database
+
     this.status = 200;
     // most likely won't return this full data here but 
     // doing for testing now
--- src/resources/venues/external/controller.js
+++ src/resources/venues/external/controller.js
@@ -3,6 +3,13 @@ import {getVenue, getVenues} from 'src/models/external/foursquare/venue';
 // External search from foursquare (most likely)
 export function* index(next) {
   const result = yield getVenues(this.request.query);
+
+  // TODO:
+  // Cache search results based on some bounding
+
+  // Cache/update cache of each individual venue
+
+  // return the results with data from internal and external
   if (result) {
     this.status = 200;
     this.body = result;
